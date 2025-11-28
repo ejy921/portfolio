101df5b23295829709dc65d57d86e9bf630ef0f1
ejy921 <ejy921@gmail.com>
2025-11-28 14:06:18 -0600

    Trial for images in 'About'

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,8 +14,8 @@ const About = () => {
         <div className='flex flex-col lg:flex-row items-center py-10 mt-8'>
 
             <div className='relative max-w-[250px] h-[270px] rounded-3xl mb-[30px] overflow-hidden'>
-                <Image src={'/profile_pic.png'} alt="user" className='rounded-2xl object-cover object-fill' priority/>
-                <Image src={'./assets/tape.png'} alt="tape" className='absolute w-[120px] h-[60px] top-0 left-1 -rotate-45 -translate-x-8'/>
+                <Image src={'/profile_pic.png'} alt="user" width={300} height={270} className='rounded-2xl object-cover object-fill' priority/>
+                <Image src={'/assets/tape.png'} alt="tape" width={120} height={60} className='absolute top-0 left-1 -rotate-45 -translate-x-8'/>
             </div>
 
             <div className='flex-1'>
@@ -29,7 +29,7 @@ const About = () => {
                     initial={{opacity: 0}}
                     whileInView={{opacity:1}}
                     transition={{duration: 0.6, delay: 0.5}}
-                    src={'main/assets/tornpaper_1.png'} alt="paper" priority fill="true" sizes="(max-width: 768px) 100vw, 580px" className='w-[400px] h-[300px] object-cover'/>
+                    src={'./assets/tornpaper_1.png'} alt="paper" priority fill="true" sizes="(max-width: 768px) 100vw, 580px" className='w-[400px] h-[300px] object-cover'/>
                     <motion.div 
                     initial={{opacity: 0}}
                     whileInView={{opacity:1}}
@@ -50,8 +50,10 @@ const About = () => {
                             initial={{y: -30, opacity: 0}}
                             whileInView={{y: 0, opacity:1}}
                             transition={{duration: 0.3, delay: 0.8}}
-                            src={'main/assets/resume_paper.png'}
+                            src={'./assets/resume_paper.png'}
                             alt="resume"
+                            width={190}
+                            height={170}
                             className='w-[190px] h-[170px] object-cover'
                         />
                         <motion.p
@@ -69,8 +71,10 @@ const About = () => {
                         initial={{y: -30, opacity: 0}}
                         whileInView={{y: 0, opacity:1}}
                         transition={{duration: 0.3, delay: 0.8}}
-                        src={assets.colored_paper_1}
+                        src={'./assets/colored_paper_1.png'}
                         alt="art link"
+                        width={160}
+                        height={170}
                         className='w-[160px] object-cover'
                     />
                     <motion.p 
@@ -101,7 +105,7 @@ const About = () => {
                     <li className='relative flex-none flex items-center justify-center w-16 sm:w-20 aspect-square p-2 sm:p-3
                     hover:translate-y-1 duration-500'
                     key={index}>
-                        <Image src={assets.taped_note_1} alt="toolnote" fill="true" sizes="(max-width: 768px) 100vw, 580px" className='object-cover items-center'/>
+                        <Image src={'./assets/taped_note_1.png'} alt="toolnote" fill="true" sizes="(max-width: 768px) 100vw, 580px" className='object-cover items-center'/>
                         <Image src={tool} alt="tool" className='absolute mt-4 mb-2 w-8 sm:w-10'/>
                     </li>
                 ))}
