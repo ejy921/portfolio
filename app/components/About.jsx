7ce62cb3d9af22b1ba0a145b7682af686d9d8541
ejy921 <ejy921@gmail.com>
2025-11-29 23:47:07 -0600

    Removed utils/basePath.js import

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { basePath } from '../utils/basePath';
 import { assets, infoList, toolsData } from '../../assets/assets'
 import Image from 'next/image'
 import React from 'react'
